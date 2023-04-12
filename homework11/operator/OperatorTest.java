fdd9b92989d2f0d3d716324ad5db254ebc0aea29
Lena <lenaaverchenko@gmail.com>
2023-04-12 21:55:11 +0200

    Changes to Comparator in Homework 11

--- homework11/operator/OperatorTest.java
+++ homework11/operator/OperatorTest.java
@@ -1,9 +1,6 @@
 package de.telran.averchenko.elena.homework11.operator;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class OperatorTest {
     public static void main(String[] args) {
@@ -24,6 +21,49 @@ public class OperatorTest {
 
         while (!priorityQueue.isEmpty()) {
             System.out.println(priorityQueue.poll());
+        }
+
+
+
+
+        Comparator<Client> clientComparator = new Comparator<Client>() {
+            @Override
+            public int compare(Client o1, Client o2) {
+                if (o1.isVip() && !o2.isVip()){
+                    return -1;
+                } else if (!o1.isVip() && o2.isVip()) {
+                    return 1;
+                } else {
+                    return 0;}
+            }
+        };
+
+        Comparator<Client> clientComparator1 = (o1,o2)-> {
+            if (o1.isVip() && !o2.isVip()) {
+                return -1;
+            } else if (!o1.isVip() && o2.isVip()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        };
+
+//        Queue<Client> priorityQueue1 = new PriorityQueue<>(clientComparator);
+
+        Queue<Client> priorityQueue1 = new PriorityQueue<>(clientComparator1);
+
+            priorityQueue1.add(new Client("Oleh", "123456789", false));
+        priorityQueue1.add(new Client("Nadia", "789456123", true));
+        priorityQueue1.add(new Client("Lena", "789123456", true));
+        priorityQueue1.add(new Client("Vasia", "321456963", false));
+        priorityQueue1.add(new Client("Igor", "741258963", false));
+        priorityQueue1.add(new Client("Kostia", "369852147", true));
+        priorityQueue1.add(new Client("Viktor", "321741852", false));
+        priorityQueue1.add(new Client("Masha", "951263487", false));
+        priorityQueue1.add(new Client("Daria", "753214896", false));
+        priorityQueue1.add(new Client("Nina", "896412357", true));
+        while (!priorityQueue1.isEmpty()){
+            System.out.println(priorityQueue1.poll());
         }
     }
 }
